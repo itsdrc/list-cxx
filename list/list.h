d834425b3a92ce9b6eda629d3ab920a951caab02
itsdrc <anexomailer@gmail.com>
2024-04-30 10:17:14 -0500

    add: push_back, push_front, front, back, size, empty

--- list/list.h
+++ list/list.h
@@ -1,8 +1,164 @@
-﻿// list.h : Include file for standard system include files,
-// or project specific include files.
+﻿#pragma once
 
-#pragma once
+#include<iostream>
+#include<utility>
 
-#include <iostream>
+template<class T>
+class list
+{
+private:
+	struct link
+	{
+		link* previous;
+		link* next;
+		link() : previous(nullptr), next(nullptr) {}
+		link(link* prev, link* nxt) : previous(prev), next(nxt) {}
 
-// TODO: Reference additional headers your program requires here.
+		link(const link& link)
+		{
+			previous = link.previous;
+			next = link.next;
+		}
+
+		link& operator=(const link& link)
+		{
+			if (this != &link)
+			{
+				previous = link.previous;
+				next = link.next;
+			}
+			return *this;
+		}
+
+		virtual ~link() {}
+	};
+
+	struct node : public link
+	{
+		T value;
+		node(const T& value_, link* prev, link* nxt)
+			: value(value_), link(prev, nxt) {}
+
+		node() = delete;
+
+		template<typename... Args>
+		node(link* prev, link* nxt, Args&&...args)
+			: link(prev, nxt), value(std::forward<Args>(args)...) {}
+	};
+
+	link head;
+	std::size_t nelms;
+
+	void deepCopy(const list& list)
+	{
+		link* aux = list.head.next;
+		while (aux != &list.head)
+		{
+			node* next = dynamic_cast<node*>(aux);
+			if (!next)
+				throw std::runtime_error("unable to casting to node");
+			push_back(next->value);
+			aux = aux->next;
+		}
+	}
+
+public:
+	list()
+	{
+		head.next = &head;
+		head.previous = &head;
+		nelms = 0;
+	}
+
+	void push_back(const T& newvalue)
+	{
+		node* new_node = new node(newvalue, head.previous, &head);
+		head.previous->next = new_node;
+		head.previous = new_node;
+		++nelms;
+	}
+
+	void push_front(const T& newvalue)
+	{
+		node* new_node = new node(newvalue, &head, head.next);
+		head.next->previous = new_node;
+		head.next = new_node;
+		++nelms;
+	}
+
+	void pop_back()
+	{
+		if (empty())
+			throw std::length_error("pop called on empty list");
+		link* last = head.previous;
+		last->previous->next = &head;
+		head.previous = last->previous;
+		delete last;
+		--nelms;
+	}
+
+	void pop_front()
+	{
+		if (empty())
+			throw std::length_error("pop called on empty list");
+		link* front = head.next;
+		head.next = front->next;
+		front->next->previous = &head;
+		delete front;
+		--nelms;
+	}
+
+	std::size_t size() const noexcept
+	{
+		return nelms;
+	}
+
+	bool empty() const noexcept
+	{
+		return nelms == 0;
+	}
+
+	T& front()
+	{
+		if (empty())
+			throw std::length_error("front called on empty list");
+		return static_cast<node*>(head.next)->value;
+	}
+
+	T& back()
+	{
+		if (empty())
+			throw std::length_error("back called on empty list");
+		return static_cast<node*>(head.previous)->value;
+	}
+
+	const T& front() const
+	{
+		if (empty())
+			throw std::length_error("front called on empty list");
+		return static_cast<node*>(head.next)->value;
+	}
+
+	const T& back() const
+	{
+		if (empty())
+			throw std::length_error("back called on empty list");
+		return static_cast<node*>(head.previous)->value;
+	}
+
+	void clear()
+	{
+		link* aux = head.next;
+		while (aux != &head)
+		{
+			node* target = static_cast<node*>(aux);
+			aux = aux->next;
+			delete target;
+		}
+
+		nelms = 0;
+		head.next = &head;
+		head.previous = &head;
+	}
+
+};
--- list/main.cpp
+++ list/main.cpp
@@ -1,12 +1,27 @@
 ﻿// list.cpp : Defines the entry point for the application.
 //
 
+#include<iostream>
 #include "list.h"
-
-using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	try
+	{
+		list<std::string> mystringlist;
+		mystringlist.push_back("Hello World");
+		mystringlist.push_back("Hello World 2");
+		mystringlist.push_back("Hello World 3");
+		mystringlist.push_back("Hello World 4");
+
+		while (!mystringlist.empty())
+		{
+			std::cout << mystringlist.front() << '\n';
+			mystringlist.pop_front();
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what();
+	}
 }
