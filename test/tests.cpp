10b20286364714c27e16bf896f921deb06333c63
itsdrc <anexomailer@gmail.com>
2024-05-01 15:48:21 -0500

    test: clear

--- test/tests.cpp
+++ test/tests.cpp
@@ -194,3 +194,40 @@ TEST_F(testResourceList, constructorByMovementshouldNotMakeCopies)
 	list<testResource> resourceList = std::move(listForTesting);
 	EXPECT_EQ(testResource::copyConstructor, 0);
 }
+
+// clear
+
+TEST(destructor_clear, noProblemUsingDestructorAfterClear)
+{
+	{
+		intlist list{ 1, 2, 3 };
+		list.clear();
+	}
+	EXPECT_EQ(1, 1);
+}
+
+TEST(destructor_clear, nelmsShouldBeZero)
+{
+	intlist list{ 1, 2, 3 };
+	list.clear();
+	std::cout << list.size();
+}
+
+TEST(destructor_clear, noProblemIfListItsEmpty)
+{
+	intlist list;
+	list.clear();
+	EXPECT_EQ(1, 1);
+}
+
+TEST(destructor_clear, canPushAfterClearWithNoProblem)
+{
+	intlist list{ 1, 2, 3 };
+	list.clear();
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	ASSERT_EQ(list.size(), 3);
+	ASSERT_EQ(list.front(), 1);
+	ASSERT_EQ(list.back(), 3);
+}
